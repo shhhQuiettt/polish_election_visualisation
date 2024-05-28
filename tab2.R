b1cfaac77ba22c33586e5bc0c482d94759c3e37c
wojbog <wojtek44wojciech@gmail.com>
2024-05-28 23:12:43 +0200

    change

--- tab2.R
+++ tab2.R
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 tab2UI <- function(id) {
   ns <- NS(id)
   splitLayout(
@@ -19,21 +18,4 @@ tab2UI <- function(id) {
               
     ))
 }
-=======
-tab2UI <- function(id) {
-    # ns <- NS(id)
-    splitLayout(
-        sidebarPanel(sliderInput("testRange",
-            label = "Boredom [%]:",
-            min = 0, max = 100,
-            value = c(0, 100)
-        )),
-        mainPanel(
-            textOutput("testText"),
-            verbatimTextOutput("clicked_row"),
-            DT::DTOutput("table"),
-        )
-    )
-}
 
->>>>>>> origin
